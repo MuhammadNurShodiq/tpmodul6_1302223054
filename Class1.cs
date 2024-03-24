84cff44a2040aafccabd2d7940e6821438d5eefc
MuhammadNurShodiq <nursodik.2004@gmail.com>
2024-03-24 20:01:46 +0700

    menambahkan class SayaTubeVideo

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302223054
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = RandomID();
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("=== Video ===");
+            Console.WriteLine($"ID: {id}, \nTitle: {title}, \nPlay Count: {playCount} \n");
+        }
+
+        private int RandomID()
+        {
+            Random random = new Random();
+            return random.Next(10000, 99999);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using tpmodul6_1302223054;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("“Tutorial Design By Contract – [Muhammad_Nur_Shodiq]");
+        video.PrintVideoDetails();
+
+        for (int i = 1; i <= 216; i++)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+        video.PrintVideoDetails();
+        Console.WriteLine("Hello, World!");
+    }
+}
